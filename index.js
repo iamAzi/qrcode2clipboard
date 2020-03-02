75d5f343ccf2c9c39ea69a1af668e29ba9e06302
Azi <spark31@126.com>
2020-03-03 01:35:18 +0800

    add options

--- index.js
+++ index.js
@@ -3,13 +3,16 @@
 const commander = require('commander');
 const task = require('./lib/core.js');
 
-commander.version('1.0.0', '-v, --version', 'output the current version');
+commander.version('1.0.0', '-v, --version', '当前版本');
 
 commander
+    .arguments('<link>')
     .usage('<string> | qr <待生成二维码的链接>')
+    .option('-m, --margin <margin>', '二维码白色边距百分比')
+    .option('-s, --size <size>', '二维码图片宽度')
     .description('使用当前链接生成二维码图片，并复制到剪贴板，可直接粘贴到聊天软件发送。')
-    .action((link) => {
-        task(link.args[0]);
+    .action((link, options) => {
+        task(link, options);
     });
 
 commander.parse(process.argv)
